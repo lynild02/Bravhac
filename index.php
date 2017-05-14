4982eaf00fa232ae5260669a50457b24cab438bc
lynild <chocobo_farm@live.com.mx>
2017-05-14 11:37:17 -0500

    Actualizado

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es" ng-app>
+  <head>
+    <title>Bravhac Corporativo</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <meta name="description" content="description">
+    <meta name="keywords" content="keywords">
+    <meta name="author" content="author">
+    <meta name="robots" content="index, follow">
+    <link rel="icon" href="img/favicon.png" type="image/x-icon">
+    <link rel="stylesheet" href="Skeleton/css/normalize.css">
+    <link rel="stylesheet" href="Skeleton/css/skeleton.css">
+    <link rel="stylesheet" href="css/master.min.css">
+  </head>
+  <body class="page-overview">
+    <div id="particles-js" class="wrapp">
+      <div class="center">
+        <div id="up"><img src="http://orangepuebla.com/test/img/logo_b.png"></div>
+        <div id="down">
+          <div class="row"><img src="img/byc.jpg" class="columns four"><img src="img/recurso 1.png" class="columns four"><img src="img/ogo.png" class="columns four"></div>
+        </div>
+      </div>
+    </div>
+  </body>
+  <script type="text/javascript" src="node_modules/jquery/dist/jquery.js" attr></script>
+  <script type="text/javascript" src="node_modules/angular/angular.js" async="async"></script>
+  <script type="text/javascript" src="node_modules/particles.js/particles.js"></script>
+  <script type="text/javascript" src="app.js"></script>
+</html>
