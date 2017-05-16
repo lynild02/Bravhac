8ff1b9217a16f24af0310f15e026e61f03168ed2
lynild <chocobo_farm@live.com.mx>
2017-05-15 21:01:50 -0500

    Primera parte terminada

--- index.php
+++ index.php
@@ -8,23 +8,36 @@
     <meta name="keywords" content="keywords">
     <meta name="author" content="author">
     <meta name="robots" content="index, follow">
+    <meta http-equiv="refresh" content="15;  url=&quot;http://orangepuebla.com&quot;">
     <link rel="icon" href="img/favicon.png" type="image/x-icon">
-    <link rel="stylesheet" href="Skeleton/css/normalize.css">
-    <link rel="stylesheet" href="Skeleton/css/skeleton.css">
+    <link rel="stylesheet" href="skeleton/css/normalize.css">
+    <link rel="stylesheet" href="skeleton/css/skeleton.css">
     <link rel="stylesheet" href="css/master.min.css">
+    <script type="text/javascript" src="node_modules/jquery/dist/jquery.js"></script>
+    <script type="text/javascript" src="node_modules/angular/angular.js" async="async"></script>
   </head>
   <body class="page-overview">
-    <div id="particles-js" class="wrapp">
-      <div class="center">
-        <div id="up"><img src="http://orangepuebla.com/test/img/logo_b.png"></div>
-        <div id="down">
-          <div class="row"><img src="img/byc.jpg" class="columns four"><img src="img/recurso 1.png" class="columns four"><img src="img/ogo.png" class="columns four"></div>
-        </div>
+    <div id="particles-js" class="wrapper"></div>
+    <header>
+      <h2>Corporativo <span>Bravhac</span></h2>
+    </header>
+    <article class="container">
+      <div class="first">
+        <figure><img src="img/bh.svg" title="bravhac logo" alt="bravhac_logo" attr></figure>
       </div>
-    </div>
+      <div class="row">
+        <figure class="columns four"><img src="img/cas.png" title="" alt="la_casona_de_joaquin">
+          <figcaption>La Casona de joaquín</figcaption>
+        </figure>
+        <figure class="columns four"><img src="img/byc.png" title="" alt="byc">
+          <figcaption>B&C Asesores Fiscales y Financieros</figcaption>
+        </figure>
+        <figure class="columns four"><img src="img/ogo.png" title="" alt="ogo_services">
+          <figcaption>Ogo Services</figcaption><a href="http://ogoser.com" title="ogo services" attr></a>
+        </figure>
+      </div>
+    </article>
   </body>
-  <script type="text/javascript" src="node_modules/jquery/dist/jquery.js" attr></script>
-  <script type="text/javascript" src="node_modules/angular/angular.js" async="async"></script>
   <script type="text/javascript" src="node_modules/particles.js/particles.js"></script>
   <script type="text/javascript" src="app.js"></script>
 </html>
